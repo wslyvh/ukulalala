ef2b4d16f8891b93468ed4e6503d0ad5264a60b6
wslyvh <wslyvh@users.noreply.github.com>
2024-08-29 13:22:40 +0200

    sendpulse fix

--- src/components/newsletter.tsx
+++ src/components/newsletter.tsx
@@ -12,7 +12,7 @@ export function Newsletter(props: Props) {
             <form className='flex flex-col gap-4 md:flex-row' target="_blank" method="post"
                 action="https://login.sendpulse.com/forms/simple/u/eyJ1c2VyX2lkIjo4Mzk2NTA3LCJhZGRyZXNzX2Jvb2tfaWQiOjc5NjcxMiwibGFuZyI6ImVuIn0=">            
                     <input className='input input-bordered input-sm w-72' name="email" type="email" placeholder='Enter your email..' required />
-                    <input type="hidden" name="sender" value="wesley@w5.gg" />
+                    <input type="hidden" name="sender" value="d2VzbGV5QHc1Lmdn" />
                     <input type="hidden" name="name" value="" />
                     <button className='btn btn-primary btn-sm w-72'>Subscribe</button>            
             </form>
